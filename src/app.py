d8f283367765f4cf4a174a61a5cf92f51178e792
mihai <mihaipop@outlook.com>
2025-01-29 07:20:03 -0800

    1

---
+++ src/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import st_tailwind as tw
+
+st.set_page_config("Streamlit Tailwind Examples")
+tw.initialize_tailwind()
+
+
+def main():
+    tw.initialize_tailwind()
+    tw.write("Grid Container", classes="text-blue-500 pb-4")
+    with tw.container(classes="grid grid-cols-4"):
+        for idx in range(1, 25):
+            st.button(f"Button {idx}")
+    tw.write("Colored Button", classes="text-purple-700 pb-7")
+    tw.button("Button", classes="bg-red-500")
+
+    with st.sidebar:
+        tw.write("Sidebar Title", classes="text-2xl font-bold text-blue-500")
+        tw.button("Home", classes="bg-blue-500 text-white w-full py-2 my-2")
+        tw.button("About", classes="bg-green-500 text-white w-full py-2 my-2")
+        tw.button("Contact", classes="bg-red-500 text-white w-full py-2 my-2")
+        tw.div
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/dashboard.py
@@ -0,0 +1,5 @@
+@ generate streamlit dashboard with sidebar and title
+def generate_dashboard():
+    st.sidebar.title('Dashboard')
+    st.title('Dashboard')
+    st.write('This is a dashboard')
